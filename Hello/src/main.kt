627155e0e1a227dc5e2ba8e54a25d02032648e57
A1esandr <ravulmirt@gmail.com>
2021-01-11 00:01:14 +0500

    Added functions

--- Hello/src/main.kt
+++ Hello/src/main.kt
@@ -2,4 +2,25 @@ fun main() {
     val hello = Hello()
     hello.run()
     hello.runName()
+
+    printMessage("Hello")                                               // 5
+    printMessageWithPrefix("Hello", "Log")                              // 6
+    printMessageWithPrefix("Hello")                                     // 7
+    printMessageWithPrefix(prefix = "Log", message = "Hello")           // 8
+    println(sum(1, 2))                                                  // 9
+    println(multiply(2, 4))
 }
+
+fun printMessage(message: String): Unit {                               // 1
+    println(message)
+}
+
+fun printMessageWithPrefix(message: String, prefix: String = "Default") {  // 2
+    println("[$prefix] $message")
+}
+
+fun sum(x: Int, y: Int): Int {                                          // 3
+    return x + y
+}
+
+fun multiply(x: Int, y: Int) = x * y                                    // 4
