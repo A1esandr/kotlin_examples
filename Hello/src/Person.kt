374419948c69ca909cbe54783ac1a2453fbb614a
A1esandr <ravulmirt@gmail.com>
2021-01-12 09:09:56 +0500

    Added infix functions

---
+++ Hello/src/Person.kt
@@ -0,0 +1,4 @@
+class Person (val name: String) {
+    val likedPeople = mutableListOf<Person>()
+    infix fun likes(other: Person) { likedPeople.add(other) }
+}
--- Hello/src/main.kt
+++ Hello/src/main.kt
@@ -9,6 +9,21 @@ fun main() {
     printMessageWithPrefix(prefix = "Log", message = "Hello")           // 8
     println(sum(1, 2))                                                  // 9
     println(multiply(2, 4))
+
+    infix fun Int.times(str: String) = str.repeat(this)        // 1
+    println(2 times "Bye ")                                    // 2
+
+    val pair = "Ferrari" to "Katrina"                          // 3
+    println(pair)
+
+    infix fun String.onto(other: String) = Pair(this, other)   // 4
+    val myPair = "McLaren" onto "Lucas"
+    println(myPair)
+
+    val sophia = Person("Sophia")
+    val claudia = Person("Claudia")
+    sophia likes claudia
+    println(sophia.likedPeople)
 }
 
 fun printMessage(message: String): Unit {                               // 1
