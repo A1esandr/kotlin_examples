afcb831aa303b8949e0291e3bedf37854bd6d73b
kurakin.as <aleksandr.kurakin@domru.ru>
2021-02-09 09:31:40 +0500

    Added returning fun example

--- Example/src/main/kotlin/funasparam.kt
+++ Example/src/main/kotlin/funasparam.kt
@@ -4,8 +4,17 @@ fun calculate(x: Int, y: Int, operation: (Int, Int) -> Int): Int {
 
 fun sum(x: Int, y: Int) = x + y
 
+fun operation(): (Int) -> Int {
+    return ::square
+}
+
+fun square(x: Int) = x * x
+
 fun main() {
     val sumResult = calculate(4, 5, ::sum)
     val mulResult = calculate(4, 5) { a, b -> a * b }
     println("sumResult $sumResult, mulResult $mulResult")
+
+    val func = operation()
+    println(func(2))
 }
